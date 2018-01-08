577f6a1c41e3e9ce0f2c0a9d4daa2aaff7d35e4c
monjurmorshed793 <monjurmorshed793@gmail.com>
2018-01-09 03:12:17 +0600

    Random changes

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { SpacialcrowdsourcingTaskModule } from './task/task.module';
+import { SpacialcrowdsourcingWorkerModule } from './worker/worker.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        SpacialcrowdsourcingTaskModule,
+        SpacialcrowdsourcingWorkerModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
